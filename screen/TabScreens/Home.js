1d126eae21e802c2bcad77e568089a2fb4f14841
Miguel A. Francisco <migufran4594@gmail.com>
2023-10-13 20:38:29 -0700

    New Pages Added for Tab Navigation

---
+++ screen/TabScreens/Home.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native"
+
+export default function Home() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Home Page </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
