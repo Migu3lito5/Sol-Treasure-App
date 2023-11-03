93d652b8b8ae2022ec7300d9c15508cb21d864e5
Miguel A. Francisco <migufran4594@gmail.com>
2023-11-03 16:55:41 -0700

    Added New Tab Page

---
+++ screen/TabScreens/Donation.js
@@ -0,0 +1,11 @@
+import { View, Text, SafeAreaView } from "react-native"
+
+export default function Donation() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text> Donation Page </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
