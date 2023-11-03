621b7fdf1a7c7a96b1172d92abd7ab5830900bf0
Miguel A. Francisco <migufran4594@gmail.com>
2023-11-03 16:23:38 -0700

    Updated Event Screen & EventCard

--- components/EventCard.js
+++ components/EventCard.js
@@ -1,41 +1,79 @@
 import { View, StyleSheet, Text, Image} from 'react-native'
 
-export default function EventCard(props) {
+export default function EventCard({imageURL, date, title, location}) {
+
   return (
     <View style={styles.card}>
       <View style={styles.imageContainer}>
-        <Image style={styles.image} source={props.imageSource} />
+        <Image style={styles.image} source={{uri: imageURL}} />
       </View>
       <View style={styles.cardInfo}>
         <View style={styles.cardDate}>
-          <Text style={styles.dateText}>12 Oct. 2023</Text>
+          <Text style={styles.dateText}>{formatDate(date)}</Text>
         </View>
-        <Text style={styles.title}>Sol Performing Strings</Text>
-        <Text style={styles.description}>
-          519 Broadway St., King City, United States
+        <Text style={styles.title}>{formatTitle(title)}</Text>
+        <Text style={styles.location}>
+          {formatLocation(location)}
         </Text>
       </View>
     </View>
   );
 }
 
+// Input Example : 2023-11-11 18:00:00
+const formatDate = (date) => {
+  const month = +date.substring(8,10);
+  const day = date.substring(5,7);
+  const year = date.substring(0,4);
+  
+  const monthNames = {
+    1: "Jan",
+    2: "Feb",
+    3: "Mar",
+    4: "Apr",
+    5: "May",
+    6: "Jun",
+    7: "Jul",
+    8: "Aug",
+    9: "Sept",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+
+  return `${day} ${monthNames[month]}. ${year}`
+}
+
+const formatLocation = (location) => {
+  const street = location.address;
+  const city = location.city;
+
+  return `${street} ${city}`
+}
+
+const formatTitle = (title) => {
+  if(title.length > 30)
+    return `${title.substring(0,27)}...`
+  
+  return title.substring(0,30);
+}
+
 const styles = StyleSheet.create({
   card: {
     flexDirection: 'row',
-    height: "auto",
     margin: 7,
+    marginTop: 10,
   },
   imageContainer: {
-    aspectRatio: 0.8, 
-    margin: 5,
-    flexDirection: 'column',
-    justifyContent: "center",
+    aspectRatio: 0.8,
+    justifyContent: 'center', // Vertically center the image
+    overflow: 'hidden', // Clip image content to rounded corners
+    borderRadius: 10, // Apply rounded corners to the image container
   },
   image: {
     width: '100%',
     height: '100%',
-    resizeMode: "cover",
-    borderRadius: 7,
+    resizeMode: 'contain', // Scale the image to fit within the container
   },
   cardInfo: {
     padding: 15,
@@ -51,11 +89,11 @@ const styles = StyleSheet.create({
     color: 'orange',
   },
   title: {
-    fontSize: 18,
+    fontSize: 19,
     fontWeight: 'bold',
     marginBottom: 10,
   },
-  description: {
+  location: {
     fontSize: 12,
   },
 });
--- screen/TabScreens/Events.js
+++ screen/TabScreens/Events.js
@@ -1,17 +1,46 @@
-import { View, Text, SafeAreaView, ScrollView } from "react-native"
-import EventCard from "../../components/EventCard"
+import React from 'react';
+import { View, Text, SafeAreaView, ScrollView, ActivityIndicator } from 'react-native';
+import EventCard from '../../components/EventCard';
+import { useQuery } from 'react-query';
+import { UseGetEventsData, fetchEventList } from '../../services/EventsAPI';
+
 
 export default function Events() {
+  const { data, isLoading } = UseGetEventsData();
+
   return (
     <SafeAreaView>
-      <Text>Hi</Text>
-    <ScrollView>
-        <EventCard imageSource={require('../../assets/Fall-Guitar-Eng.png')}/>
-        <EventCard imageSource={require('../../assets/SPS-Eng.png')}/>
-        <EventCard imageSource={require('../../assets/Fall-Guitar-Eng.png')}/>
-        <EventCard imageSource={require('../../assets/Any-Ofrenda-Eng-1.png')}/>
-        <EventCard imageSource={require('../../assets/Fall-Guitar-Eng.png')}/>
-    </ScrollView>
+      {
+        isLoading ? (<ActivityIndicator size="large" color="orange" />) 
+          : data ? (
+            <View>
+              {data.events.map((event) => {
+                // Check if event and event.image exist before accessing properties
+                if (event && event.image && event.image.url) {
+                  return (
+                    <EventCard
+                      key={event.id}
+                      title={event.title}
+                      imageURL={event.image.url}
+                      location={event.venue}
+                      date={event.utc_start_date}
+                    />
+                  );
+                } else {
+                  return null; // Handle cases where event or event.image.url is missing
+                  // might have to add a default image if that is the case
+                }
+              })}
+            </View>
+          )
+        : <Text>Error</Text>
+      }
     </SafeAreaView>
-  )
+  );
 }
+
+
+
+ 
+
+
